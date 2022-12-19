bac7ded988dcb5e531831c314f731a84063bcf29
Archuser555 <98958302+archuser555@users.noreply.github.com>
2022-12-19 07:56:14 -0500

    Fix issues
    yep...

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import math
 pi = math.pi
 points = [pi*2, pi/2, pi, (4*pi)/3, (5*pi)/3] # points...
 steps = [3, 0, 2, 4, 1, 3] # Here is the cordinates from x & y that will form the star shape
+size = 100 #size of star
 x = []
 y = []
 i = 0
@@ -16,12 +17,18 @@ while True:
     y.append(math.sin(points[i]))
     i += 1
 
+#setup something's
 t = turtle.Turtle()
-t.down()
+turtle.Screen().bgcolor("red")
+t.pencolor("green")
+t.pensize(20)
+t.up()
 i = 0
 #Doing Moves...
 while i < len(steps):
-    t.goto(x[steps[i]]*50, y[steps[i]]*50)
+    if(i == 1):
+    	t.down() #this fix a small issue while drawing the start
+    t.goto(x[steps[i]]*size, y[steps[i]]*size)
     i += 1
     print(i)
 
